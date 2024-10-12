c61f66f4d4198ed1093c17adbb5c6ef28ace1e70
Truong Tu <120169852+vhhung1105@users.noreply.github.com>
2024-10-12 16:09:12 +0700

    Commit

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './Component/SearchScreen';
 import Home from './Screen/Home';
 import OrderScreen from './Screen/OrderScreen';
 
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Order' component={OrderScreen} />
+        <Stack.Screen name='Search' component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
