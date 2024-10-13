16179b675ec858ddcd236441d1fc9475803a3938
Truong Tu <tutvph14566@fpt.edu.vn>
2024-10-13 11:36:31 +0700

    backlaicode

--- App.js
+++ App.js
@@ -3,15 +3,25 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './Screen/LoginScreen';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './Screen/Home';
-
+import RegisterScreen from './Screen/RegisterScreen';
+import SplashScreen from './Screen/SplashScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Home' component={Home} />
+      <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{headerShown: false}}>
+      <Stack.Screen
+                            name="Login"
+                            component={LoginScreen}
+                            options={{ title: 'HELLO' }}
+                        />
+                        <Stack.Screen
+                            name="Register"
+                            component={RegisterScreen}
+                            options={{ title: 'Đăng ký' }}
+                        />
+                         <Stack.Screen component={SplashScreen} name='SplashScreen' />
       </Stack.Navigator>
     </NavigationContainer>
   );
