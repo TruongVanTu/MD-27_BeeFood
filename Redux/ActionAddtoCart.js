247826140e349b720dac7d3b593e9b5f0e38f35e
sangnv <sangnv19093@gmail.com>
2024-11-02 16:22:24 +0700

    deleteproduct-redux

--- Redux/ActionAddtoCart.js
+++ Redux/ActionAddtoCart.js
@@ -1,8 +1,15 @@
-import { ADD_PRODUCT_TO_CART } from "./Type";
+import { ADD_PRODUCT_TO_CART, DELETE_PRODUCT } from "./Type";
 
 export const addproducttocart = (product)=>{
     return {
         type: ADD_PRODUCT_TO_CART ,
         payload: product
+    }
+}
+
+export const deleteproduct = (productid)=>{
+    return{
+        type:DELETE_PRODUCT , 
+        payload:productid
     }
 }
