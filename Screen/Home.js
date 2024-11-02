5669d268ad0ff414018c245cbc1c88a4002d9c8c
sangnv <sangnv19093@gmail.com>
2024-11-03 04:21:43 +0700

    update giao diện Home

--- Screen/Home.js
+++ Screen/Home.js
@@ -6,6 +6,7 @@ import Toast from 'react-native-toast-message';
 import * as Location from 'expo-location'; // Import Location từ expo-location
 import { URL } from '../const/const';
 import { useFocusEffect } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
 
 const { width, height } = Dimensions.get('window');
 
@@ -33,12 +34,11 @@ const HeaderHome = ({ navigation }) => {
 
 
   return (
-    <View style={{ marginTop: 30 }}>
+    <View style={{ marginTop: 30, marginBottom: 10 }}>
       <View style={{
         flexDirection: 'row',
         alignItems: 'center',
         padding: 10,
-        backgroundColor: '#f5f5f5',
         borderRadius: 10,
         shadowColor: '#000',
         shadowOpacity: 0.1,
@@ -75,58 +75,24 @@ const HeaderHome = ({ navigation }) => {
           />
         </TouchableOpacity>
       </View>
-
-      <Text
-        style={{
-          fontSize: 25,
-          color: '#319AB4',
-          fontWeight: 'bold',
-          marginStart: 15,
-          marginTop: 20, // Thêm khoảng cách trên cho thoáng
-          textAlign: 'center'
-        }}>
-        Welcome to BeeFood!
-      </Text>
-
-      <View style={{flexDirection: 'row', 
-    alignItems: 'center', 
-    marginVertical: 20, }}>
-        <TouchableOpacity onPress={() => { navigation.navigate('Search') }}>
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#f1f1f1', marginLeft: 10, marginRight: 10
+      }}>
+        <Icon name='search' size={20} color='gray' style={{ marginLeft: 15 }} />
+        <TouchableOpacity style={{ flex: 1 }} onPress={() => { navigation.navigate('Search') }}>
           <TextInput
             onFocus={() => { navigation.navigate('Search') }}
             style={{
-              width: 0.8 * width,
               height: 40,
-              borderRadius: 15,
-              borderWidth: 1,
-              borderColor: 'black',
-              marginLeft: 15,
+              borderColor: '#f1f1f1',
               paddingLeft: 10,
+              marginRight: 15
             }}
             placeholder="Nhập từ khóa tìm kiếm"
           />
         </TouchableOpacity>
-
-
-        <View
-          style={{
-            flex: 1,
-            // width: 40,
-            borderRadius: 15,
-            marginLeft: 5,
-            marginRight: 15,
-            height: 40,
-            backgroundColor: '#319AB4',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <TouchableOpacity onPress={() => { navigation.navigate('Search') }} >
-            <Image
-              source={require('./../Image/search.png')}
-              style={{ width: 25, height: 25 }}
-            />
-          </TouchableOpacity>
-        </View>
       </View>
     </View>
   )
@@ -217,7 +183,7 @@ const Restaurant = ({ navigation }) => {
   }, [])
 
   return (
-    <View>
+    <View style={{ paddingBottom: 15 }}>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 15, marginVertical: 8 }}>
         <Text style={{ fontWeight: 'bold', fontSize: 20, color: '#616161' }}>Nhà hàng quanh đây</Text>
         <TouchableOpacity onPress={() => navigation.navigate('AllRestaurant')}>
@@ -226,20 +192,17 @@ const Restaurant = ({ navigation }) => {
       </View>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {datarestauran.map((data, index) =>
-          <View style={{ width: 250 }} key={data._id}>
+          <View style={{ width: 150 }} key={data._id}>
             <TouchableOpacity onPress={() => navigation.navigate('Restaurant', { restaurant: data._id })}>
-              <View style={{ marginLeft: 15 }}>
-                <Image source={{ uri: data.image }} style={{ width: 0.58 * width, height: 0.2 * height, borderTopLeftRadius: 10, borderTopRightRadius: 10 }} />
+              <View style={{ marginLeft: 15, marginRight: 15 }}>
+                <Image source={{ uri: data.image }} style={{ width: 0.33 * width, height: 0.2 * height }} />
               </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#ADD8E6', marginLeft: 15, width: 0.58 * width, height: 0.08 * height }}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#f1f1f1', marginLeft: 15, width: 0.33 * width, height: 0.08 * height, borderWidth:0.5, borderColor:'#ccc' }}>
                 <View style={{ flexDirection: 'column', padding: 8 }}>
-                  <Text style={{ fontWeight: 'bold', fontSize: 20, color: '#000000' }}>{data.name}</Text>
-                  <Text style={{ fontWeight: 'bold', color: '#000000' }}>{data.timeon} AM - {data.timeoff} PM</Text>
+                  <Text numberOfLines={1} ellipsizeMode='tail' style={{ fontWeight: 'bold', fontSize: 20, color: '#000000', fontSize:16 }}>{data.name}</Text>
+                  <Text style={{ fontSize:11, color: '#000000' }}>{data.timeon} AM - {data.timeoff} PM</Text>
                   <Text style={{ fontWeight: 'bold', color: '#000000' }}>{data.adress}</Text>
                 </View>
-                <TouchableOpacity onPress={() => navigation.navigate('Restaurant', { restaurant: data._id })} style={{ marginLeft: 'auto', backgroundColor: '#FFFFFF', width: 0.06 * width, alignItems: 'center', justifyContent: 'center', height: 0.025 * height, borderRadius: 20, marginTop: 20, marginRight: 10 }} >
-                  <Image source={require('./../Image/right_arrow.png')} style={{ width: 15, height: 15 }} />
-                </TouchableOpacity>
               </View>
             </TouchableOpacity>
           </View>
@@ -286,8 +249,8 @@ const Goiymonan = ({ navigation }) => {
   );
 
   return (
-    <View style={{ margin: 15 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+    <View style={{ backgroundColor: '#f1f1f1' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', padding: 15 }}>
         <Text style={{ fontWeight: 'bold', fontSize: 20, color: '#616161' }}>
           Gợi ý dành cho bạn
         </Text>
@@ -297,14 +260,14 @@ const Goiymonan = ({ navigation }) => {
       </View>
       <ScrollView>
         {datamonangoiy.map((data, index) => (
-          <View key={data._id || index} style={{ backgroundColor: '#FFE4C4', marginTop: 8, borderRadius: 10 }}>
+          <View key={data._id || index} style={{ backgroundColor: 'white', marginBottom: 10 }}>
             <TouchableOpacity
               style={{ margin: 15, flexDirection: 'row', alignItems: 'center' }}
               onPress={() => navigation.navigate('ProductDetail', { product: data })}
             >
               <Image
                 source={{ uri: data.image }}
-                style={{ borderWidth: 1, width: width * 0.25, height: width * 0.25, borderRadius: 10 }}
+                style={{ width: width * 0.25, height: width * 0.25 }}
               />
               <View style={{ flexDirection: 'column', paddingLeft: 10, marginLeft: 10 }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 15, color: '#000000' }}>
@@ -341,19 +304,26 @@ const Home = ({ navigation }) => {
     setRefreshing(false); // Sau khi tải xong, đặt lại refreshing
   }, []);
   return (
-    <View style={{ backgroundColor: 'white' }}>
+    <View style={{ backgroundColor: '#f1f1f1' }}>
+      <View style={{ backgroundColor: 'white' }}>
+        <HeaderHome navigation={navigation} />
+      </View>
       <ScrollView showsVerticalScrollIndicator={false}
-        StickyHeaderComponent={HeaderHome}
+        // StickyHeaderComponent={HeaderHome}
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }>
-
-        <HeaderHome navigation={navigation} />
         <SliderHome />
-        <Menu navigation={navigation} />
+        <View style={styles.bgr}>
+          <Menu navigation={navigation} />
+        </View>
+        <View style={styles.bgr}>
+          <Restaurant navigation={navigation} />
+        </View>
 
-        <Restaurant navigation={navigation} />
-        <Goiymonan navigation={navigation} />
+        <View style={styles.bgr}>
+          <Goiymonan navigation={navigation} />
+        </View>
 
       </ScrollView>
     </View>
@@ -392,4 +362,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '500',
   },
+  bgr: {
+    backgroundColor: 'white',
+    marginTop: 12,
+    marginBottom: 12
+  }
 }); 
