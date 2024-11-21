fdaf1528bc5f979e1f207dab0ed48bc62ec618c0
Truong Tu <tutvph14566@fpt.edu.vn>
2024-11-21 13:11:38 +0700

    code thong tin ca nhan

--- UserInfor.js
+++ UserInfor.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
 import {
     View,
     Text,
@@ -7,7 +8,6 @@ import {
     TextInput,
     Alert,
     TouchableOpacity,
-    SafeAreaView,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -15,34 +15,57 @@ import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { RadioButton } from 'react-native-paper';
 import ToolBar from './components/ToolBar';
 
+import { URL } from './const/const';
+
 export default function UserInfor() {
+    
     const [userInfo, setUserInfo] = useState(null);
     const [isEditing, setIsEditing] = useState(false);
     const [editedUserInfo, setEditedUserInfo] = useState({});
+    const [token, setToken] = useState(null);
+    const [userId, setUserId] = useState(null);
     const [selectedGender, setSelectedGender] = useState(null);
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const navigation = useNavigation();
 
     useEffect(() => {
-        getStoredUserInfo();
+        getStoredUserId();
     }, []);
 
-    const getStoredUserInfo = async () => {
+    const getStoredUserId = async () => {
         try {
-            const storedUserInfo = await AsyncStorage.getItem('userInfo');
-            if (storedUserInfo) {
-                const parsedUserInfo = JSON.parse(storedUserInfo);
-                setUserInfo(parsedUserInfo);
-                setEditedUserInfo(parsedUserInfo);
-                setSelectedGender(parsedUserInfo.gender);
-            } else {
-                // Nếu không có thông tin người dùng, khởi tạo đối tượng rỗng
-                setUserInfo({});
-                setEditedUserInfo({});
-                setIsEditing(true); // Cho phép người dùng nhập thông tin
+            const storedToken = await AsyncStorage.getItem('token');
+            const storedUserId = await AsyncStorage.getItem('_id');
+            setToken(storedToken);
+            setUserId(storedUserId);
+
+            if (storedUserId) {
+                fetchUserInfo(storedUserId);
             }
         } catch (error) {
-            console.error('Lỗi khi lấy thông tin người dùng từ AsyncStorage:', error);
+            console.error('Lỗi khi lấy ID người dùng từ AsyncStorage:', error);
+        }
+    };
+
+    const fetchUserInfo = async (user_Id) => {
+        try {
+            const response = await fetch(URL + `api/users/info/${user_Id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                const userData = await response.json();
+                setUserInfo(userData);
+                setEditedUserInfo(userData);
+                setSelectedGender(userData.gender);
+            } else {
+                console.error('Lỗi khi lấy thông tin người dùng từ máy chủ.');
+            }
+        } catch (error) {
+            console.error('Lỗi khi lấy thông tin người dùng:', error);
         }
     };
 
@@ -69,60 +92,172 @@ export default function UserInfor() {
             birthday: date,
         });
     };
+    
 
+    // const pickImage = async () => {
+    //     if (isEditing) {
+    //         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //         if (status !== 'granted') {
+    //             alert('Quyền truy cập thư viện ảnh bị từ chối!');
+    //             return;
+    //         }
+    //
+    //         const result = await ImagePicker.launchImageLibraryAsync({
+    //             mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    //             allowsEditing: true,
+    //             aspect: [4, 3],
+    //             quality: 1,
+    //         });
+    //
+    //         if (!result.cancelled) {
+    //             const formData = new FormData();
+    //             formData.append('avatar', {
+    //                 uri: result.uri,
+    //                 type: 'image/jpeg',
+    //                 name: 'avatar.jpg',
+    //             });
+    //
+    //             try {
+    //                 const response = await fetch(URL + `api/users/update-avatar/${userId}`, {
+    //                     method: 'POST',
+    //                     headers: {
+    //                         Accept: 'application/json',
+    //                         'Content-Type': 'multipart/form-data',
+    //                         Authorization: `Bearer ${token}`,
+    //                     },
+    //                     body: formData,
+    //                 });
+    //
+    //                 if (response.status === 200) {
+    //                     const data = await response.json();
+    //                     setEditedUserInfo({
+    //                         ...editedUserInfo,
+    //                         avatar: data.avatarPath,
+    //                     });
+    //                     Alert.alert('Thông báo', 'Cập nhật ảnh đại diện thành công');
+    //                 } else {
+    //                     Alert.alert('Lỗi', 'Cập nhật ảnh đại diện thất bại');
+    //                 }
+    //             } catch (error) {
+    //                 console.error('Lỗi khi tải ảnh lên:', error);
+    //             }
+    //         }
+    //     } else {
+    //         alert('Bạn cần bật chế độ chỉnh sửa để thay đổi ảnh đại diện.');
+    //     }
+    // };
+    
     const handleSaveChanges = async () => {
+       // Kiểm tra nếu mật khẩu cũ hoặc mật khẩu mới không có giá trị
+    if (!editedUserInfo.currentPassword) {
+        return Alert.alert('Lỗi', 'Bạn cần nhập mật khẩu cũ để xác nhận.');
+    }
+
+    if (!editedUserInfo.password) {
+        return Alert.alert('Lỗi', 'Bạn cần nhập mật khẩu mới.');
+    }
+
         try {
-            // Lưu thông tin người dùng vào AsyncStorage
-            await AsyncStorage.setItem('userInfo', JSON.stringify(editedUserInfo));
-            Alert.alert('Thông báo', 'Cập nhật thông tin thành công');
-            setIsEditing(false);
-            // Cập nhật lại thông tin hiển thị
-            setUserInfo(editedUserInfo);
+            const updateData = {
+                username: editedUserInfo.username,
+                password: editedUserInfo.password,
+                phone: editedUserInfo.phone,
+                currentPassword: editedUserInfo.currentPassword, // Mật khẩu cũ
+                gender: editedUserInfo.gender,
+                birthday: editedUserInfo.birthday,
+            };
+    
+            const response = await fetch(URL + `api/users/update/${userId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(updateData),
+            });
+    
+            console.log('Response status:', response.status);
+            const responseData = await response.json();
+            console.log('Response data:', responseData);
+    
+            if (response.status === 200) {
+                Alert.alert('Thông báo', 'Cập nhật thông tin thành công');
+                setIsEditing(false);
+                fetchUserInfo(userId); // Làm mới thông tin hiển thị
+            } else {
+                 Alert.alert('Lỗi', responseData.error || 'Cập nhật thất bại');
+            }
         } catch (error) {
-            console.error('Lỗi khi cập nhật thông tin:', error);
-            Alert.alert('Lỗi', 'Cập nhật thất bại');
+           console.error('Lỗi khi cập nhật:', error);
+           Alert.alert('Lỗi', 'Đã xảy ra lỗi khi cập nhật thông tin.');
         }
     };
+    
 
-    const handleGenderChange = (value) => {
-        setSelectedGender(value);
-        setEditedUserInfo({
-            ...editedUserInfo,
-            gender: value,
-        });
-    };
 
     const fieldsToDisplay = [
-        { key: 'username', label: 'Tên người dùng' },
-        { key: 'phone', label: 'Số điện thoại' },
         {
-            key: 'gender',
-            label: '',
-            render: () => (
-                <View style={styles.genderContainer}>
-                    <Text style={{ color: 'gray', fontSize: 15, marginTop: 10 }}>
-                        Giới tính
-                    </Text>
-                    <RadioButton.Group
-                        onValueChange={(value) => handleGenderChange(value)}
-                        value={selectedGender}
-                    >
-                        <View style={styles.radioContainer}>
-                            <RadioButton.Item label="Nam" value="Nam" />
-                            <RadioButton.Item label="Nữ" value="Nữ" />
-                        </View>
-                    </RadioButton.Group>
-                </View>
-            ),
+            key: 'username',
+            label: 'Tên người dùng',
+            render: () =>
+                (
+                    <Text style={styles.text}>{userInfo.username}</Text>
+                ),
+        },
+        {
+            key: 'phone',
+            label: 'Số điện thoại',
+            render: () =>
+                isEditing ? (
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Nhập số điện thoại"
+                        onChangeText={(text) =>
+                            setEditedUserInfo({ ...editedUserInfo, phone: text })
+                        }
+                        value={editedUserInfo.phone || ''}
+                    />
+                ) : (
+                    <Text style={styles.text}>{userInfo.phone}</Text>
+                ),
+        },
+        {
+            key: 'currentPassword',
+            label: 'Mật khẩu cũ',
+            render: () =>
+                isEditing ? (
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Nhập mật khẩu cũ"
+                        secureTextEntry
+                        onChangeText={(text) =>
+                            setEditedUserInfo({ ...editedUserInfo, currentPassword: text })
+                        }
+                        value={editedUserInfo.currentPassword || ''}
+                    />
+                ) : null,
+        },
+        {
+            key: 'password',
+            label: 'Xác nhận mật khẩu mới',
+            render: () =>
+                isEditing ? (
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Xác nhận mật khẩu mới"
+                        secureTextEntry
+                        onChangeText={(text) =>
+                            setEditedUserInfo({ ...editedUserInfo, password: text })
+                        }
+                        value={editedUserInfo.password || ''}
+                    />
+                ) : null,
         },
         {
             key: 'birthday',
-            label: '',
+            label: 'Ngày sinh',
             render: () => (
                 <View>
-                    <Text style={{ color: 'gray', fontSize: 15, padding: 8 }}>
-                        Ngày sinh
-                    </Text>
                     {isEditing ? (
                         <TouchableOpacity onPress={showDatePicker}>
                             <Text style={styles.text}>
@@ -133,7 +268,7 @@ export default function UserInfor() {
                         </TouchableOpacity>
                     ) : (
                         <Text style={styles.text}>
-                            {userInfo && userInfo.birthday
+                            {userInfo.birthday
                                 ? formatBirthday(userInfo.birthday)
                                 : ''}
                         </Text>
@@ -148,63 +283,38 @@ export default function UserInfor() {
             ),
         },
     ];
-
+    
     return (
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             <ToolBar title="Thông tin cá nhân" onBackPress={() => navigation.goBack()} />
+             <ScrollView contentContainerStyle={styles.scrollContainer}>
             {userInfo ? (
                 <View style={styles.horizontalContainer}>
-                    {/* Nếu muốn hiển thị avatar, bạn có thể bật phần này lên */}
-                    {/* <View style={styles.avatarContainer}>
-                        <TouchableOpacity onPress={pickImage}>
-                            {editedUserInfo.avatar ? (
-                                <Image
-                                    source={{ uri: editedUserInfo.avatar }}
-                                    style={styles.avatarImage}
-                                />
-                            ) : (
-                                <Text style={{ fontSize: 20, color: '#ff0000' }}>
-                                    Chọn ảnh đại diện
-                                </Text>
-                            )}
-                        </TouchableOpacity>
-                    </View> */}
                     <View style={styles.infoContainer}>
                         {fieldsToDisplay.map((field) => (
                             <View key={field.key} style={styles.textInfo}>
-                                <Text style={{ color: 'gray', fontSize: 15, marginTop: 10 }}>
+                                <Text style={{ color: 'gray', fontSize: 15, marginTop: 10}}>
                                     {field.label}
                                 </Text>
-                                {isEditing && field.key !== 'username' ? (
-                                    field.render ? (
-                                        field.render()
-                                    ) : (
-                                        <TextInput
-                                            style={styles.text}
-                                            value={editedUserInfo[field.key]}
-                                            onChangeText={(text) =>
-                                                setEditedUserInfo({
-                                                    ...editedUserInfo,
-                                                    [field.key]: text,
-                                                })
-                                            }
-                                        />
-                                    )
+                                {isEditing ? (
+                                    field.render && field.render()
                                 ) : (
                                     <Text style={styles.text}>
-                                        {userInfo && userInfo[field.key]
-                                            ? userInfo[field.key]
-                                            : ''}
+                                        {field.render
+                                            ? field.render()
+                                            : userInfo && userInfo[field.key]
+                                                ? userInfo[field.key]
+                                                : ''}
                                     </Text>
                                 )}
                             </View>
                         ))}
-                        <View>
+                        <View >
                             <TouchableOpacity
                                 style={styles.btn}
                                 onPress={() => setIsEditing(!isEditing)}
                             >
-                                <Text style={{ fontSize: 20, color: '#fff' }}>
+                                <Text style={{ fontSize: 20, color: '#ff0000' }}>
                                     {isEditing ? 'Hủy' : 'Chỉnh sửa thông tin'}
                                 </Text>
                             </TouchableOpacity>
@@ -220,7 +330,8 @@ export default function UserInfor() {
                     <Text style={styles.buttonText}>LƯU</Text>
                 </TouchableOpacity>
             )}
-        </SafeAreaView>
+              </ScrollView>
+        </View>
     );
 }
 
@@ -251,17 +362,18 @@ const styles = StyleSheet.create({
     textInfo: {
         borderBottomWidth: 0.3,
         borderBottomColor: 'gray',
-        padding: 10,
+        padding:10,
         backgroundColor: '#f1f8fc',
-        marginBottom: 15,
-        borderRadius: 20,
-        borderWidth: 1,
+        marginBottom:15,
+        borderRadius:20,
+        borderWidth:1
     },
     text: {
         fontSize: 18,
+
     },
     button: {
-        backgroundColor: '#FF9900', // Màu nền
+        backgroundColor: '#FF9900', // Màu nền xanh
         padding: 10,
         borderRadius: 5, // Bo góc
         margin: 10,
@@ -289,5 +401,13 @@ const styles = StyleSheet.create({
     radioContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+    },
+    textInput: {
+        fontSize: 18,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+        marginBottom: 10,
+        paddingHorizontal: 8,
+        color: 'black',
     },
 });
